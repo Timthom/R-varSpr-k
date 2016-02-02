3bd55fb26a0c6fd81e275febc3c0732c864e78fd
Thomas Månsson <thomasmonsson@gmail.com>
2016-02-02 11:50:26 +0100

    Initial Commit

---
+++ RovarSprak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RovarSprak
+//
+//  Created by Thomas on 2016-02-02.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RovarSprak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RovarSprak
+//
+//  Created by Thomas on 2016-02-02.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
